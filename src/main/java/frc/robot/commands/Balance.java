0734b588113861991bf8d450e2a6b929eb7f4c5c
Lewis Mojica <mojicalewis@gmail.com>
2023-02-11 19:05:07 -0400

    show gyro measurement on shuffleboard

--- src/main/java/frc/robot/commands/Balance.java
+++ src/main/java/frc/robot/commands/Balance.java
@@ -1,8 +1,13 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.GyroSubsystem;
+import edu.wpi.first.wpilibj.smartdashboard.*;
+
+
+
 
 public class Balance extends CommandBase {
   private final DriveSubsystem dSubsystem;
@@ -17,20 +22,19 @@ public class Balance extends CommandBase {
   @Override
   public void execute() {
     if (gSubsystem.getAngle() > 5) {
-      dSubsystem.arcadeDrive(0, .3);
+      dSubsystem.arcadeDrive(0, .1);
     } else if (gSubsystem.getAngle() > -5) {
-      dSubsystem.arcadeDrive(0, -.3);
+      dSubsystem.arcadeDrive(0, -.1);
     } else {
       dSubsystem.stopMotors();
     }
+    SmartDashboard.putNumber("ang",gSubsystem.getAngle());
+
+    
   }
 
   @Override
   public boolean isFinished() {
-    if (gSubsystem.getAngle() > -5 && gSubsystem.getAngle() < 5) {
-      return false;
-    } else {
-      return true;
-    }
+    return (gSubsystem.getAngle() > -5 && gSubsystem.getAngle() < 5);
   }
 }

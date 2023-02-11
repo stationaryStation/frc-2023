cdabe1234e695f71f0219fae3a9286eb63dbf5ed
Lewis Mojica <mojicalewis@gmail.com>
2023-02-11 19:49:29 -0400

    Fix Balance command

--- src/main/java/frc/robot/commands/Balance.java
+++ src/main/java/frc/robot/commands/Balance.java
@@ -13,6 +13,7 @@ public class Balance extends CommandBase {
   private final DriveSubsystem dSubsystem;
   private final GyroSubsystem gSubsystem;
 
+
   public Balance(DriveSubsystem drivetrain, GyroSubsystem gyro) {
     dSubsystem = drivetrain;
     gSubsystem = gyro;
@@ -21,20 +22,26 @@ public class Balance extends CommandBase {
 
   @Override
   public void execute() {
-    if (gSubsystem.getAngle() > 5) {
-      dSubsystem.arcadeDrive(0, .1);
-    } else if (gSubsystem.getAngle() > -5) {
-      dSubsystem.arcadeDrive(0, -.1);
+    if (gSubsystem.getAngle() > 2.0) {
+      SmartDashboard.putBoolean("go left", true);
+      SmartDashboard.putBoolean("go right",false);
+      dSubsystem.arcadeDrive(0, 1);
+
+    } else if (gSubsystem.getAngle() < -2.0) {
+      SmartDashboard.putBoolean("go left", false);
+      SmartDashboard.putBoolean("go right",true);
+      dSubsystem.arcadeDrive(0, -1);
+
     } else {
-      dSubsystem.stopMotors();
+
     }
     SmartDashboard.putNumber("ang",gSubsystem.getAngle());
 
-    
   }
 
   @Override
   public boolean isFinished() {
-    return (gSubsystem.getAngle() > -5 && gSubsystem.getAngle() < 5);
+    return false;
+    //return (gSubsystem.getAngle() > -5 && gSubsystem.getAngle() < 5);
   }
 }

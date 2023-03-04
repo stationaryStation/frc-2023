66cb66ae35ce66773b265a290f2f77aafe2b5f27
Lewis Mojica <mojicalewis@gmail.com>
2023-03-04 16:14:29 -0400

    Revert changes on robot container file

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,9 +9,7 @@ import frc.robot.commands.goUp;
 import frc.robot.commands.toggleX;
 import frc.robot.commands.toggleGrab;
 import frc.robot.commands.stopYArmMovement;
-import frc.robot.commands.camData;
 import frc.robot.subsystems.ArmSubsystem;
-import frc.robot.subsystems.CamSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.GyroSubsystem;
 
@@ -20,7 +18,6 @@ public class RobotContainer {
     private final DriveSubsystem robotDrivetrain = new DriveSubsystem();
     private final GyroSubsystem gyroscope = new GyroSubsystem();
     private final ArmSubsystem arm = new ArmSubsystem();
-    private final CamSubsystem cam = new CamSubsystem();
 
     // Initialize the driver's controller
     CommandXboxController driverController = new CommandXboxController(DriverContstants.driverControllerPort);
@@ -32,9 +29,6 @@ public class RobotContainer {
     private toggleX fordward = new toggleX(arm);
     private toggleGrab grab = new toggleGrab(arm);
     private stopYArmMovement stopYArm = new stopYArmMovement(arm);
-
-    private stopXArmMovement stopXArm = new stopXArmMovement(arm);
-    private camData sendCamData = new camData(cam);
 
 
     /**
@@ -55,7 +49,6 @@ public class RobotContainer {
      * See https://docs.wpilib.org/en/stable/docs/software/commandbased/binding-commands-to-triggers
      */
     private void configureButtonBindings() {
-        driverController.y().toggleOnTrue(sendCamData);
         driverController.a().onTrue(fordward);
         driverController.b().onTrue(grab);
         driverController.povUp().onTrue(upCommand);
